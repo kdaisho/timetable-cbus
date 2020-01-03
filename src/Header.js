c6be253eed9a67473137957eb1262be28e688d07
daisho <daishokomiyama@gmail.com>
2020-01-03 07:00:16 -0500

    Replacing class component with functional component as no state to handle

--- src/Header.js
+++ src/Header.js
@@ -1,46 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Header extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <header>
-                <div className="container">
-                    <h1 className="title is-5 has-text-white">
-                        北谷町コミュニティバス
-                    </h1>
-                    {this.props.directions.map(dir => (
-                        <div className="buttons" key={dir.id}>
-                            <span className="buttons-title">{dir.dirName}</span>
-                            <button
-                                className="button is-light is-small"
-                                onClick={this.props.toggleDirection}
-                                data-dirname={dir.dirName}
-                                data-dirid={dir.dirId}
-                                data-day={dir.weekday.name}
-                                data-dayid={dir.weekday.value}
-                            >
-                                {dir.weekday.name}
-                            </button>
-                            <button
-                                className="button is-light is-small"
-                                onClick={this.props.toggleDirection}
-                                data-dirname={dir.dirName}
-                                data-dirid={dir.dirId}
-                                data-day={dir.weekend.name}
-                                data-dayid={dir.weekend.value}
-                            >
-                                {dir.weekend.name}
-                            </button>
-                        </div>
-                    ))}
-                </div>
-            </header>
-        );
-    }
-}
+const Header = props => {
+    const { directions, toggleDirection } = props;
+    return (
+        <header>
+            <div className="container">
+                <h1 className="title is-5 has-text-white">
+                    北谷町コミュニティバス
+                </h1>
+                {directions.map(dir => (
+                    <div className="buttons" key={dir.id}>
+                        <span className="buttons-title">{dir.dirName}</span>
+                        <button
+                            className="button is-light is-small"
+                            onClick={toggleDirection}
+                            data-dirname={dir.dirName}
+                            data-dirid={dir.dirId}
+                            data-day={dir.weekday.name}
+                            data-dayid={dir.weekday.value}
+                        >
+                            {dir.weekday.name}
+                        </button>
+                        <button
+                            className="button is-light is-small"
+                            onClick={toggleDirection}
+                            data-dirname={dir.dirName}
+                            data-dirid={dir.dirId}
+                            data-day={dir.weekend.name}
+                            data-dayid={dir.weekend.value}
+                        >
+                            {dir.weekend.name}
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </header>
+    );
+};
 
 export default Header;
