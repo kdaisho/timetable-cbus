1fd8869e9af37766522926182312b3743d27cfb5
daisho <daishokomiyama@gmail.com>
2020-01-03 06:54:06 -0500

    Replacing class component with functional component as there's no state to handle for the component

--- src/Table.js
+++ src/Table.js
@@ -1,47 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Table extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const dir = this.props.directionId;
-        const day = this.props.dayId;
-        return (
-            <div className="container">
-                <h2 className="title is-5">
-                    {this.props.direction} - {this.props.day}
-                </h2>
-                <div className="container-table section">
-                    <div className="table-scroll">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <td className="fixed">Bus Stops</td>
-                                    <td colSpan="6">&nbsp;</td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.props.arrivals[dir][day].map(row => {
-                                    return (
-                                        <tr key={row.id}>
-                                            <td className="fixed">
-                                                {row.name}
-                                            </td>
-                                            {row.hours.map(h => (
-                                                <td key={h}>{h}</td>
-                                            ))}
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
+const Table = props => {
+    const { arrivals, direction, day, directionId, dayId } = props;
+    return (
+        <div className="container">
+            <h2 className="title is-5">
+                {direction} - {day}
+            </h2>
+            <div className="container-table section">
+                <div className="table-scroll">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <td className="fixed">Bus Stops</td>
+                                <td colSpan="6">&nbsp;</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arrivals[directionId][dayId].map(row => {
+                                return (
+                                    <tr key={row.id}>
+                                        <td className="fixed">{row.name}</td>
+                                        {row.hours.map(h => (
+                                            <td key={h}>{h}</td>
+                                        ))}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        );
-    }
-}
-
+        </div>
+    );
+};
 export default Table;
